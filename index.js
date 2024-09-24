0bba32e99aec728fef45b4250a8f95be05e2b8d9
Ariadna <ariadnakaori@hotmail.com>
2024-09-24 15:53:41 -0500

    function

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-console.log("Hola Navilovers!")
-
 const h1 = document.querySelector("h1")
 h1.innerText = "Productos"
 
-
 let cardsArray = [];
-for (let card = 1; card <= 9; card++){
-    const cards = `<div class="card" style="width: 18rem;">
-    <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${card}.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title">Card title ${card}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-    </div>`
+function cardsProductos(){
+    for (let card = 1; card <= 9; card++){
+        const cards = `<div class="card" style="width: 18rem;">
+        <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${card}.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Card title ${card}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>`
 
-    cardsArray.push(cards)
+        cardsArray.push(cards)
+    }
 }
+cardsProductos()    
 document.querySelector("section").innerHTML = cardsArray.join().replaceAll(",", "");
