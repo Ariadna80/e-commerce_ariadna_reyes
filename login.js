8c591e69664bfc1fffc1ced5282f7f12cda017d1
Ariadna <ariadnakaori@hotmail.com>
2024-10-30 16:16:36 -0500

    Login

---
+++ login.js
@@ -0,0 +1,22 @@
+const UserLogin = {email:"kyra@gmail.com", password:"1284"};
+
+const form = document.querySelector("form");
+const message = document.querySelector("#formMessage");
+
+function clearMessage (){
+    message.innerText = "";
+}
+form.elements.email.addEventListener("input",clearMessage);
+form.elements.password.addEventListener("input",clearMessage);
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if(UserLogin.email === form.elements.email.value && UserLogin.password === form.elements.password.value ){
+        localStorage.setItem("email",form.elements.email.value)
+        window.location.href = "./index.html";
+    }else{
+        message.innerText = "Credenciales inválidas";
+        form.elements.email.value = "";
+        form.elements.password.value = "";
+    }
+})
